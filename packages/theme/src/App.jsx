d8e2db80be275a8c3eb981144e6833b6aff02b31
root <root@anas.local>
2025-09-17 18:35:01 +0500

    first commit

---
+++ packages/theme/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='text-[50px] font-serif text-100xl font-bold text-red-600' >Dev Anas </div>
+  )
+}
+
+export default App
